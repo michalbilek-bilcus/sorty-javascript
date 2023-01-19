98e607782bac71b25f99123d6ac7d1922ccd267e
michalbilek-bilcus <tadymichal@gmail.com>
2023-01-19 22:22:41 +0100

    select sort

--- selectsort.js
+++ selectsort.js
@@ -3,7 +3,7 @@ document.getElementById("selectText").innerHTML = selectNumbers.toString();
 function selectSort() {
     for (let i = 0; i < selectNumbers.length; i++) {
         let prom = i;
-        for (let j = i + 1; j < selectNumbers.length; j++){
+        for (let j = i + 1; j < selectNumbers.length; j++) {
             if (selectNumbers[prom] > selectNumbers[j]) {
                 prom=j;
             }
