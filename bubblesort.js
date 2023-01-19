d50ff5f431aa42e92927fb1abe20dcfd9ce1517e
michalbilek-bilcus <tadymichal@gmail.com>
2023-01-19 22:23:58 +0100

    bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,10 +1,10 @@
 let bubbleNumbers = [5,1,4,8,2,10,7];
 document.getElementById("bubbleText").innerHTML = bubbleNumbers.toString();
 function bubbleSort() {
-    for (let i = 0; i < bubbleNumbers.length - 1; i++) {
+    for (let i=0; i < bubbleNumbers.length-1; i++) {
         let prom = false;
         if (prom === false) {
-            for (let j = 0; j < bubbleNumbers.length - i - 1; j++) {
+            for (let j=0; j < bubbleNumbers.length - i - 1; j++) {
                 if (bubbleNumbers[j] > bubbleNumbers[j+1]) {
                     prom = true;
                     let save = bubbleNumbers[j];
