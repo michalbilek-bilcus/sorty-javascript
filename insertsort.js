46825d802406c1f5c61d2348d1679a7dea112fd3
michalbilek-bilcus <tadymichal@gmail.com>
2023-01-26 12:18:56 +0100

    insert sort

--- insertsort.js
+++ insertsort.js
@@ -4,12 +4,11 @@ function insertSort() {
     for (let i=0; i < insertNumbers.length-1; i++) {
         let j=i;
         let pom=insertNumbers[i+1];
-
         while(insertNumbers[i] > pom && j >= 0){
             insertNumbers[j+1]=insertNumbers[j];
             j--;
         }
         insertNumbers[j+1]=pom;
-        document.getElementById("insertText").innerHTML = insertNumbers.toString();
     }
+    document.getElementById("insertText").innerHTML = insertNumbers.toString();
 }
